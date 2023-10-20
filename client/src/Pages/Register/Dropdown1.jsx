d8e640cc39fa9331d6a29184132be846c7009c1e
prachi samuel <prachisamuel28@gmail.com>
2023-10-20 20:08:33 +0530

    dropdown

---
+++ client/src/Pages/Register/Dropdown1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Select from 'react-select';
+
+export from function Dropdown1() {
+    const options = [
+        { label: "red", value: 1},
+        { label: "green", value: 2},
+        { label: "yellow", value: 3},
+    ]
+    return (
+        
+    )
+}
