5f4b063648cad0ce5071bc862054b54907cd2f8c
Prachi Samuel <137616292+prachisamuel@users.noreply.github.com>
2024-01-06 22:32:10 +0530

    Delete client directory

--- client/src/Component/RightPostContainer/Rightbar.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Rightbar = () => {
-    return (
-        <div>Rightbar</div>
-    )
-}
-
-export default Rightbar;
--- client/src/redux/constants/postConstants.js
+++
@@ -1,2 +0,0 @@
-export const GET_POSTS_SUCCESS = 'getPostsSuccess'
-export const GET_POSTS_FAIL = 'getPostsFail'
